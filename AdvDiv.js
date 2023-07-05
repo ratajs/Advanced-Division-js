a1bc70d2ccb109bd7ed81fe05f69a9faab890e90
Šimon Rataj <ratajs@ratajs.cz>
2023-07-05 11:23:49 +0200

    Support for the r2 argument

--- AdvDiv.js
+++ AdvDiv.js
@@ -1,17 +1,19 @@
-function advdiv(n1, n2, r1, rstr1, rstr2) {
+function advdiv(n1, n2, r1, r2, rstr1, rstr2) {
 	var over = false;
 	var carry = 0, newcarry = 0, rcount = -1, x, y, d;
 	var n1string, n2string, res = "", result;
+	var times10;
 	var carries = [];
 	var n1s;
-	var sign;
+	var n1mc, n2mc, m1, m2;
+	var sign, n1m, n2m;
 	var n1s1;
 
 	if(!rstr1 && rstr1!=="")
 		rstr1 = "[";
 	if(!rstr2 && rstr2!="")
 		rstr2 = "]";
-	if(n2==0)
+	if(n2==0 && r2==0)
 		return false;
 	if(!r1)
 		r1 = 0;
@@ -19,15 +21,38 @@ function advdiv(n1, n2, r1, rstr1, rstr2) {
 	n1 = Math.abs(n1);
 	n2 = Math.abs(n2);
 	r1 = Math.abs(r1);
-	var times10 = function(nstring) {
+	r2 = Math.abs(r2);
+	n1string = n1.toString();
+	n2string = n2.toString();
+
+	if(r2!=0) {
+		n1m = n1string.replace(".", "");
+		n2m = n2string.replace(".", "");
+
+		n1mc = parseInt(n1m+r1.toString()) - parseInt(n1m);
+		n2mc = parseInt(n2m+r2.toString()) - parseInt(n2m);
+
+		if(n1string.indexOf(".") > -1)
+			m1 = Math.pow(10, n1string.length - n1string.indexOf(".") - 1 + r1.toString().length) - Math.pow(10, n1string.length - n1string.indexOf(".") - 1);
+		else
+			m1 = Math.pow(10, r1.toString().length) - 1;
+
+		if(n2string.indexOf(".") > -1)
+			m2 = Math.pow(10, n2string.length - n2string.indexOf(".") - 1 + r2.toString().length) - Math.pow(10, n2string.length - n2string.indexOf(".") - 1);
+		else
+			m2 = Math.pow(10, r2.toString().length) - 1;
+
+		return advdiv(n1mc * m2, m1 * n2mc, 0, 0, rstr1, rstr2);
+	};
+
+	times10 = function(nstring) {
 		if(nstring.indexOf(".") > -1 && nstring.indexOf(".")==nstring.length - 2)
 			return nstring.replace(".", "");
 		if(nstring.indexOf(".") > -1)
 			return nstring.split(".")[0] + nstring.split(".")[1].split("")[0] + "." + nstring.split(".")[1].slice(1);
 		return nstring + "0";
 	};
-	n1string = n1.toString();
-	n2string = n2.toString();
+
 	while(n2string.indexOf(".") > -1) {
 		if(n1string.indexOf(".") < 0) {
 			n1string+= r1.toString()[0];
